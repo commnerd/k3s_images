d7bad84c10367fd57307aa6904bb7bf3233856cd
Michael J. Miller <commnerd@gmail.com>
2021-10-20 11:56:21 -0600

    Change permissions on files in /root/.ssh directory

--- edge-reverse-tunnel/connect.sh
+++ edge-reverse-tunnel/connect.sh
@@ -6,4 +6,6 @@ cp -fR /tmp/.ssh /root/.ssh
 
 chown -fR root:root /root/.ssh
 
+chmod 600 /root/.ssh/*
+
 ssh -i /root/.ssh/${AUTH_KEY_FILE} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -R ${BIND_PORT}:${DEST}:${DEST_PORT} -N ${USER}@${SERVER}
